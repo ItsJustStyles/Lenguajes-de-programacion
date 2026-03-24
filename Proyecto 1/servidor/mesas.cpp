396d11dc62457afeb203f347ed7a221f3a6e01fc
JosimarAS <arayajosimar68@gmail.com>
2026-03-23 22:55:59 -0600

    Update de mesas.cpp
    Literalmente se actualizaron las mesas XD
    
    También se añadió la validación a este archivo

--- Proyecto 1/servidor/mesas.cpp
+++ Proyecto 1/servidor/mesas.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "mesas.h"
 
+bool mesaValida(int numeroMesa) {
+    return restaurante.totalMesas > 0 && numeroMesa >= 1 && numeroMesa <= restaurante.totalMesas;
+}
+
 void configurarMesas() {
-    std::cout << "Nuevo numero de mesas: ";
+    std::cout << "Número de mesas actual: " << restaurante.totalMesas << "\n";
+    std::cout << "Nuevo número de mesas: ";
     std::cin >> restaurante.totalMesas;
     std::cin.ignore();
-    std::cout << "[Servidor] Mesas configuradas correctamente\n";
+    std::cout << "[Servidor] Número de mesas actualizado a: " << restaurante.totalMesas << "\n";
 }

c8709efb73c26b6dee2eeec0bed7f7047025b283
ItsJustStyles <lacayopicado@gmail.com>
2026-06-28 15:32:55 -0600

    Se implemento la interfaz, la fase de grupos y la clasificacion a la eliminatoria

--- Proyecto 3/src/learned_facts.pl
+++ Proyecto 3/src/learned_facts.pl
@@ -5,3 +5,9 @@
 :- assertz(hecho_aprendido(parte_de,tarjeta,computadora)).
 :- assertz(es_un(lenguaje_de_programacion,software)).
 :- assertz(hecho_aprendido(es_un,lenguaje_de_programacion,software)).
+:- assertz(es_un(casa,lugar_donde_viven_personas)).
+:- assertz(hecho_aprendido(es_un,casa,lugar_donde_viven_personas)).
+:- assertz(concepto(aruña,'propiedad de gato')).
+:- assertz(hecho_aprendido(concepto,aruña,'propiedad de gato')).
+:- assertz(es_un(elefante,mamifero)).
+:- assertz(hecho_aprendido(es_un,elefante,mamifero)).
